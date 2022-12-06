74f2e7893eebaaf232cec15325d23cc6e92c5cb4
Gyan Prakash <106138051+Gyan18@users.noreply.github.com>
2022-12-06 18:51:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-    user_data = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update -y
               sudo apt install docker.io -y
